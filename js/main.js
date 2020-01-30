2237c8bd678074b68b506f39e7d382f83d7e647e
Tom Lee <tomtleee@gmail.com>
2020-01-30 07:58:44 -0500

    Set up side panel

---
+++ js/main.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoidHRsZWU0IiwiYSI6ImNrNjBwd3pmaTBhbmwzZ2w2bDdyeDcyYW0ifQ.1cscNlBBjYXqedUM8c4ELw';
+
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    center: [-90.0, 44.8], // starting position [lng, lat],
+    zoom: 6 // starting zoom
+});
