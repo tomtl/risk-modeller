c89e1bb0c8f6688bda227fd40ca0654a5631f677
Tom Lee <tomtleee@gmail.com>
2020-02-08 11:42:57 -0500

    Calc the linear regression

--- js/main.js
+++ js/main.js
@@ -46,58 +46,75 @@ map.on('load', function(){
             var distance = 10;
             var weight = 2;
             var idwOptions = {gridType: "hex", property: "nitr_con", units: "kilometers", weight: weight};
-            var wellGrid = turf.interpolate(wellPoints, distance, idwOptions);
+            var nitrateGrid = turf.interpolate(wellPoints, distance, idwOptions);
 
-            // add nitrate grid to map
-            // map.addLayer({
-            //     "id": "wells-grid",
-            //     "source": {
-            //         "type": "geojson",
-            //         "data": wellGrid
-            //     },
-            //     "type": "fill",
-            //     "paint": {
-            //         "fill-color": [ "interpolate",
-            //             ["linear"],
-            //             ["get", "nitr_con"],
-            //                 0, "#000000",
-            //                 4, "#a1307e",
-            //                 12, "yellow"
-            //     ],
-            //     "fill-outline-color": "#ffffff",
-            //     "fill-opacity": 0.90
-            //     }
-            // });
-        });
-
-        // interpolate the census tracts cancer rates data to a grid
-        $.getJSON("data/cancer_tracts/cancer_tract_centroids.geojson", function(tractPolygons){
-            // interpolate the tracts
-            // https://turfjs.org/docs/#interpolate
-            var distance = 10;
-            var weight = 2.0;
-            var idwOptions = {gridType: "square", property: "canrate", units: "kilometers", weight: weight};
-            var tractGrid = turf.interpolate(tractPolygons, distance, idwOptions);
-
+            // // add nitrate grid to map
             map.addLayer({
-                "id": "tracts-grid",
+                "id": "nitrate-grid",
                 "source": {
                     "type": "geojson",
-                    "data": tractGrid
+                    "data": nitrateGrid
                 },
                 "type": "fill",
                 "paint": {
                     "fill-color": [ "interpolate",
                         ["linear"],
-                        ["get", "canrate"],
-                            0.0, "#440154",
-                            0.1, "#20928c",
-                            0.3, "yellow"
+                        ["get", "nitr_con"],
+                            0, "#000000",
+                            4, "#a1307e",
+                            12, "yellow"
                 ],
                 "fill-outline-color": "#ffffff",
                 "fill-opacity": 0.90
                 }
             });
+
+            // interpolate the census tracts cancer rates data to a grid
+            $.getJSON("data/cancer_tracts/cancer_tract_centroids.geojson", function(tractPolygons){
+                // interpolate the tracts
+                // https://turfjs.org/docs/#interpolate
+                var distance = 10;
+                var weight = 2.0;
+                var idwOptions = {gridType: "point", property: "canrate", units: "kilometers", weight: weight};
+                var tractGrid = turf.interpolate(tractPolygons, distance, idwOptions);
+
+                // map.setLayoutProperty('nitrate-grid', 'visibility', 'none')
+                // map.addLayer({
+                //     "id": "tracts-grid",
+                //     "source": {
+                //         "type": "geojson",
+                //         "data": tractGrid
+                //     },
+                //     "type": "fill",
+                //     "paint": {
+                //         "fill-color": [ "interpolate",
+                //             ["linear"],
+                //             ["get", "canrate"],
+                //                 0.0, "#440154",
+                //                 0.1, "#20928c",
+                //                 0.3, "yellow"
+                //     ],
+                //     "fill-outline-color": "#ffffff",
+                //     "fill-opacity": 0.90
+                //     }
+                // });
+
+                // regression time
+                // build the array of nitrate values and cancer rates
+                var valuesArray = [];
+                for (var i in nitrateGrid.features){
+                    var nitrateValue = Math.round(nitrateGrid.features[i].properties.nitr_con *1000)/1000;
+                    var nitratePoint = turf.centerOfMass(nitrateGrid.features[i]);
+                    var canrateValue = Math.round(turf.nearestPoint(nitratePoint, tractGrid).properties.canrate *1000000)/1000000;
+                    valuesArray.push([nitrateValue, canrateValue]);
+                };
+
+                // carry out the linear regression
+                var linearRegression = ss.linearRegression(valuesArray);
+                console.log(linearRegression);
+            });
         });
+
+
     });
 });
